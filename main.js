18c5141ac0bd402ddd1e00f580120039e0b00e5e
D-Dietrich <89669360+D-Dietrich@users.noreply.github.com>
2021-09-19 21:31:21 -0500

    Add files via upload
    Changed first main to complexMain. Created simpler main.

--- main.js
+++ main.js
@@ -1,73 +1,40 @@
 //Daniel Dietrich - Assignment 2 - INF 651
-//Question 1:
+//Question 1
 let myNumber;
-//Question 2:
-myNumber = window.prompt("Enter the number 5: " );
 
-while(myNumber != 5)
-{
-    myNumber = window.prompt("You did not enter the number 5. Please try again: ")
-}
-console.log("Q1 & Q2: myNumber now equals " + myNumber + ".");
+//Question 2
+myNumber = 5;
 
-//Question 3:
+//Question 3
 let myOtherNumber;
-myOtherNumber = window.prompt("Enter the number 5 again: " );
+myOtherNumber = 5;
 
-while(myOtherNumber != 5)
-{
-    myOtherNumber = window.prompt("You did not enter the number 5. Please try again: ")
-}
-console.log("Q3: myOtherNumber now equals " + myOtherNumber + ".");
-
-//Question 4:
+//Question 4
 let myTotal;
-myTotal = myNumber * myOtherNumber;
+myTotal = myNumber*myOtherNumber;
 
-console.log("Q4: myTotal is equal to " + myTotal + ".");
-
-//Question 5:
+//Question 5
 let myName = "";
 let myCombo = "";
-let temp = Number(myNumber) + Number(myOtherNumber);
-myCombo = temp; //Number converted to string
 
-//myCombo = myNumber + myOtherNumber; 
-//**This is in the event that we were supposed to observe that the two numbers are converted to strings before adding, giving us "55"
-console.log("Q5: myCombo is equal to " + myCombo + ".");
+myCombo == (Number (myNumber) + Number (myOtherNumber));
 
-//Question 6:
+//Question 6
 let myOtherCombo;
 myOtherCombo = Number(myNumber) + Number(myOtherNumber);
-console.log("Q6: myOtherCombo is equal to " + myOtherCombo + ".");
 
-//Question 7:
-let myRemainder = myNumber % 5;
-console.log("Q7: myRemainder is equal to " + myRemainder + ".");
+//Question 7
+let myRemainder;
+myRemainder = myNumber % 5;
 
-//Question 8:
+//Question 8
 let i;
-i = window.prompt("Enter the number 2: " );
+i = 2;
 
-while(i != 2)
-{
-    i = window.prompt("You did not enter the number 2. Please try again: ")
-}
-console.log("Q8: i now equals " + i + ".");
-
-//Question 9:
+//Question 9
 let int;
-int = window.prompt("Enter the number 4: " );
+int = 4;
 
-while(int != 4)
-{
-    int = window.prompt("You did not enter the number 4. Please try again: ")
-}
-console.log("Q9: int now equals " + int + ".");
-
-//Question 10:
-let myBoolean = false;
-if(myBoolean)
-console.log("Q10: myBoolean now equals true.");
-else
-console.log("Q10: myBoolean now equals false.");
+//Question 10
+let myBoolean;
+myBoolean = false;
